7b8f4439517ce94517366c97d76aa862c5811036
shanx2 <shanshan.cao@live.nl>
2018-07-12 17:13:01 +0200

    gentle push fo* ite 1

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
+import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
+
+const routesArray: Routes = [
+  {},
+  {}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponentComponent,
+    MyMovieComponentComponent
   ],
   imports: [
     BrowserModule,
